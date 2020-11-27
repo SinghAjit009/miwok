cdee330c56cf3a7de4fecc0a5598c435b93c8482
Ajit <ajit18009@iiitd.ac.in>
2020-11-28 01:24:16 +0530

    before audio

--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -30,7 +30,7 @@ public class PhrasesActivity extends AppCompatActivity {
         ArrayList<Word> words = new ArrayList<Word>();
         words.add(new Word("Where are you going?", "minto wuksus",0));
         words.add(new Word("What is your name?", "tinnә oyaase'nә",0));
-        words.add(new Word("My name is.", "oyaaset...",0));
+        words.add(new Word("My name is...", "oyaaset...",0));
         words.add(new Word("How are you feeling?", "michәksәs?",0));
         words.add(new Word("I'm feeling good.", "kuchi achit",0));
         words.add(new Word("Are you coming?", "әәnәs'aa?",0));
